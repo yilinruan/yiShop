46fbbf0e703b90da254fb4c2ac930f7f15b85117
yilin <yilinruan@gmail.com>
2026-05-26 23:53:16 -0700

    node update v24 branch

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-    mongoose
-    .connect(process.env.DB)
-    .then(con => console.log(`connected to database with HOST: ${con.connection.host}`))
-}
+const connectDatabase = async () => {
+  try {
+    const con = await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`Connected to MongoDB: ${con.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+
+    process.exit(1);
+  }
+};
 
 module.exports = connectDatabase;
